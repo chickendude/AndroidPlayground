6f16b314fffa3773178fdf0260c456110f31bf18
chickendude <pokemaster103@gmail.com>
2018-01-01 17:57:29 -0500

    Starting/Stopping timer working.

--- app/src/main/java/ch/ralena/activitypractice/fragments/TimerFragment.java
+++ app/src/main/java/ch/ralena/activitypractice/fragments/TimerFragment.java
@@ -5,28 +5,44 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.os.Messenger;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 import ch.ralena.activitypractice.R;
 import ch.ralena.activitypractice.services.TimerService;
 
 public class TimerFragment extends BaseFragment implements ServiceConnection {
+	public static final int MSG_GET_TIME = 0;
+
+	private FragmentActivity activity;
 	private TextView timeValue;
 
+	private TimerService timerService;
 	private Messenger messenger;
+	private TimerHandler handler;
 	private boolean isBoundToService = false;
 
 	@Nullable
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		activity = getActivity();
+
+		// handler to regularly query service
+		handler = new TimerHandler(this);
+
 		rootView = inflater.inflate(R.layout.fragment_timer, container, false);
 		timeValue = rootView.findViewById(R.id.timeValue);
 
@@ -35,7 +51,21 @@ public class TimerFragment extends BaseFragment implements ServiceConnection {
 
 		startStopButton.setOnClickListener(view -> {
 			if (isBoundToService) {
+				if (timerService.isRunning()) {
+					timerService.stopTimer();
+					startStopButton.setText("Start");
+					handler.removeMessages(MSG_GET_TIME);
+				} else {
+					timerService.startTimer();
+					startStopButton.setText("Stop");
+					handler.sendEmptyMessage(MSG_GET_TIME);
+				}
+			}
+		});
 
+		resetButton.setOnClickListener(view -> {
+			if (isBoundToService) {
+				timerService.resetTimer();
 			}
 		});
 
@@ -47,7 +77,7 @@ public class TimerFragment extends BaseFragment implements ServiceConnection {
 		super.onStart();
 		// start timer service
 		Intent intent = new Intent(getContext(), TimerService.class);
-		getActivity().bindService(intent, this, Context.BIND_AUTO_CREATE);
+		activity.bindService(intent, this, Context.BIND_AUTO_CREATE);
 	}
 
 	@Override
@@ -55,21 +85,45 @@ public class TimerFragment extends BaseFragment implements ServiceConnection {
 		super.onStop();
 		// stop service
 		if (isBoundToService) {
-			getActivity().unbindService(this);
+			activity.unbindService(this);
 			isBoundToService = false;
+		}
+	}
+
+	public void showTime() {
+		if (isBoundToService) {
+			float time = timerService.getTimeInSeconds();
+			timeValue.setText(String.format(Locale.getDefault(), "%.2f seconds", time));
 		}
 	}
 
 	@Override
 	public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
 		// get our messenger and show that we're bound to the service
+		Log.d("TAG", "Bound");
 		isBoundToService = true;
-		messenger = new Messenger(iBinder);
+		timerService = ((TimerService.TimerBinder) iBinder).getService();
 	}
 
 	@Override
 	public void onServiceDisconnected(ComponentName componentName) {
 		// update service bound boolean
+		Log.d("TAG", "Unbound");
 		isBoundToService = false;
 	}
+
+	public class TimerHandler extends Handler {
+		TimerFragment fragment;
+
+		public TimerHandler(TimerFragment fragment) {
+			this.fragment = fragment;
+		}
+
+		@Override
+		public void handleMessage(Message msg) {
+			fragment.showTime();
+			sendEmptyMessageDelayed(MSG_GET_TIME, 25);
+		}
+	}
+
 }
--- app/src/main/java/ch/ralena/activitypractice/services/TimerService.java
+++ app/src/main/java/ch/ralena/activitypractice/services/TimerService.java
@@ -2,37 +2,74 @@ package ch.ralena.activitypractice.services;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.Handler;
+import android.os.Binder;
 import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 public class TimerService extends Service {
-	public Messenger messenger = new Messenger(new TimerHandler());
+	public final static int START_TIMER = 0;
+	public final static int STOP_TIMER = 1;
+	public final static int RESET_TIMER = 2;
 
-	private class TimerHandler extends Handler {
-		@Override
-		public void handleMessage(Message msg) {
-			Log.d("tag", "handling message");
+	TimerBinder binder;
+
+	long startTime = 0;
+	long endTime = 0;
+	long duration = 0;
+	boolean isRunning = false;
+
+	// get a copy of the service so we can run its methods from fragment
+	public class TimerBinder extends Binder {
+		public TimerService getService() {
+			return TimerService.this;
 		}
 	}
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		// set up thread
+		binder = new TimerBinder();
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
+		isRunning = false;
 		return START_STICKY;
 	}
 
 	@Nullable
 	@Override
 	public IBinder onBind(Intent intent) {
-		return messenger.getBinder();
+		return binder;
+	}
+
+	public void startTimer() {
+		if (!isRunning) {
+			isRunning = true;
+			startTime = System.currentTimeMillis();
+		}
+	}
+
+	public void stopTimer() {
+		if (isRunning) {
+			isRunning = false;
+			duration += System.currentTimeMillis() - startTime;
+		}
+	}
+
+	public void resetTimer() {
+		startTime = 0;
+		endTime = 0;
+		duration = 0;
+	}
+
+	public boolean isRunning() {
+		return isRunning;
+	}
+
+	public float getTimeInSeconds() {
+		long currentTime = System.currentTimeMillis();
+		long timePassed = currentTime - startTime + duration;
+		return ((float) timePassed) / 1000;
 	}
 }
